1e1d2962942630ab3224bc9d69d1275a0b4f49c6
J4HR <92270891+J4HR@users.noreply.github.com>
2023-09-18 06:46:12 -0600

    avances endpoint
    json implementado 
    Rediseño de la base de datos
    Ya compila XD

--- Endpoint/app.py
+++ Endpoint/app.py
@@ -1,53 +1,144 @@
+#Se importa configuraciones Json
+import json
 #Se importa de flask los objetos que ocuparemos 
 from flask import Flask
 from flask import request
 #Se importa configoraciones de desarrollo 
 from config import DevConfig 
 #Se importa configuraciones de la base de datos 
-from BDManager import DatabaseManager as db_CRUD
+#from BDManager import DatabaseManager as db_CRUD
 from models import db
 from models import User
 from models import Login
+from models import Paciente
+from models import Doctor
+from models import Familiar
 #Se importa configuraciones de la base de datos y comunicaciones
-from Comunicacion.Connections import RPC as epRPC
+#from Comunicacion.Connections import RPC as epRPC
 #se declara un gestor de Base de datos mediante RPC
-endPoindRPC=epRPC()
-endPoindRPC.appJoined(db_CRUD)#se agregan entrada de instruciones para manejar la base de datos por RPC
+#endPoindRPC=epRPC()
+#endPoindRPC.appJoined(db_CRUD)#se agregan entrada de instruciones para manejar la base de datos por RPC
 #se inicializa la API 
 app=Flask(__name__) 
 app.config.from_object(DevConfig)#se agrega la configuracion
-
-def endPoint_http():
-    #busqueda en la base de datos
-    login= Login.query.filter_by(password = request.args.get( 'password' )).first()
-    if not login is None :
+def http_Delete():
+    login= Login.query.filter_by(email = request.args.get( 'email' )).first()
+    if not login is None and login.checkPassword(request.args.get( 'Password' )):
         user = User.query.filter_by(id = login.ID_user).first()
-        if(request.args.get( 'email' )):
-            return user.Login[0].email #encriptar
-        elif(request.args.get( 'historial' )):
-            return user.Historial #encriptar json
-        elif(request.args.get( 'ondas' )):
-            return user.Ondas #encriptar json
-        elif(request.args.get( 'Doctor' )):
-            return user.Doctor #encriptar
-        elif(request.args.get( 'Pacienta' )):
-            return user.Pacientes #encriptar json
-        elif(request.args.get( 'Familiares' )):
-            return user.Familiares#encriptar json
-        print(request)#control de petición
-        print(request.args)#control parametros de petición 
-        print(request.args.get('P1'))#control parametro1 de petición 
+        db.session.delete(user)
+        db.session.commit()
+
+def http_Update():
+    login= Login.query.filter_by(email = request.args.get( 'email' )).first()
+    if not login is None and login.checkPassword(request.args.get( 'Password' )):
+        user = User.query.filter_by(id = login.ID_user).first()
+        if (request.args.get( 'username' )):
+            user.username=request.args.get( 'username' )
+        if (request.args.get( 'apellidos' )):
+            user.apellidos=request.args.get( 'apellidos' )
+        if (request.args.get( 'fechaNacimiento' )):
+            user.fechaNacimiento=request.args.get( 'fechaNacimiento' )
+        if (request.args.get( 'email' )):
+            login.setEmail(request.args.get( 'Email' ))
+        if (request.args.get( 'Password' )):
+            login.setPassword(request.args.get( 'Password' ))
+        
+        id_new = request.args.get( 'new' )
+
+        if user.tipo == 0 and id_new:
+            paciente = Paciente.query.filter_by(ID_user = user.id).first()
+            if not paciente is None:
+                fam = Familiar.query.filter_by(ID_user = id_new).first()
+                if not fam is None:
+                    fam.setPaciente(id_new,paciente.cedulaDoc)
+                
+        elif user.tipo == 1 and request.args.get( 'new' ):
+            doc = Doctor.query.filter_by(ID_user = user.id).first()
+            if not doc is None:
+                paciente = Paciente.query.filter_by(ID_user = id_new).first()
+                if not paciente is None:
+                    paciente.setDoctor(doc.cedula_profecional)
+
+        db.session.commit()
+                
+def http_Create():
+    user = User(
+        username=request.args.get( 'username' ),
+        apellidos=request.args.get( 'apellidos' ),
+        tipo=request.args.get( 'tipo' ),
+        fechaNacimiento=request.args.get( 'fechaNacimiento' )
+    )
+    login = Login()
+    login.setEmail(request.args.get( 'Email' ))
+    login.setPassword(request.args.get( 'Password' ))
+    db.session.add(user)
+    db.session.add(login)
+    
+
+    if user.tipo == 0 :
+        paciente=Paciente(user.id)
+        db.session.add(paciente)
+
+    elif user.tipo == 1 and not request.args.get( 'cedula' ) is None:
+        doc=Doctor(request.args.get( 'cedula' ),user.id)
+        db.session.add(doc)
+
+    elif user.tipo == 2:
+        fam=Familiar(user.id)
+        db.session.add(fam)
+        
+    db.session.commit()
+
+def http_Read():
+    print(request)#control de petición
+    print(request.args)#control parametros de petición 
+    print(request.args.get('P1'))#control parametro1 de petición 
+    #busqueda en la base de datos
+    login= Login.query.filter_by(email = request.args.get( 'email' )).first()
+    if not login is None and login.checkPassword(request.args.get( 'Password' )):
+        user = User.query.filter_by(id = login.ID_user).first()
+
+        if(request.args.get( 'user' )):
+            return json.dumps(
+                user.to_dict(), 
+                sort_keys=True, 
+                indent=4
+            )
+        elif(request.args.get( 'Pacientes' )) and user.tipo == 0 :
+            return json.dumps(
+                user.userPaciente.to_dict() , 
+                sort_keys=True, 
+                indent=4
+            )
+        elif(request.args.get( 'Doctor' ))and user.tipo == 1:
+            return json.dumps(
+                user.userDoctor.to_dict() , 
+                sort_keys=True, 
+                indent=4
+            )
+        elif(request.args.get( 'Familiares' ))and user.tipo == 2:
+            return json.dumps(
+                user.userFamiliar.to_dict(), 
+                sort_keys=True, 
+                indent=4
+            )
 
     else:
         pass
     
-    return login.email#encriptar
-
+    return #encriptar
+'''elif(request.args.get( 'historial' )):
+            return user.Historial #encriptar json
+        elif(request.args.get( 'ondas' )):
+            return user.Ondas #encriptar json'''
 if __name__=='__main__': 
     db.init_app(app)#inicia el gestor db de la api
     
     with app.app_context():
         db.create_all()# crea la base de datos si no existe
-        app.add_url_rule('/UserData',view_func=endPoint_http)#consulta de la tabla usuario y login con api de flask
+        app.add_url_rule('/DeleData',view_func=http_Delete)#consulta de la tabla usuario y login con api de flask
+        app.add_url_rule('/UpdtData',view_func=http_Update)
+        app.add_url_rule('/CrteData',view_func=http_Create)
+        app.add_url_rule('/ReadData',view_func=http_Read)
         #link?data1=dat
-    app.run(port=5000)#app.run(debug=True,port=puerto)#Depuración
+    app.run(host='0.0.0.0',port=5000)#app.run(debug=True,port=puerto)#Depuración
--- Endpoint/models.py
+++ Endpoint/models.py
@@ -9,33 +9,66 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "User"
-    id = db.Column(db.Integer,unique = True, primary_key=True)
-    username = db.Column(db.String(50),unique = True,nullable=False)
-    apellidos = db.Column(db.String(50),unique = True,nullable=False)
-    tipo= db.Column(db.Integer,unique = False, primary_key=True)
+    id = db.Column(db.Integer,unique = True,nullable=False, primary_key=True)
+    username = db.Column(db.String(50),unique = False,nullable=False)
+    apellidos = db.Column(db.String(50),unique = False,nullable=False)
+    tipo= db.Column(db.Integer,unique = False,nullable=False)
     fechaNacimiento=db.Column(db.DateTime,default=datetime.datetime.now)
 
     Login=relationship("Login")
-    Historial=relationship("Historial")
-    Ondas=relationship("Onda")
-    Doctor=relationship("Doctor")
-    Pacientes=relationship("Paciente")
-    Familiares=relationship("Familiar")
+    Doctor_tipo=relationship("Doctor")
+    Paciente_tipo=relationship("Paciente")
+    Familiar_tipo=relationship("Familiar")
 
-    def __init__(self, username):
+    @property
+    def userPaciente(self):
+        return self.Paciente_tipo[0]
+    @property
+    def userDoctor(self):
+        return self.Doctor_tipo[0]
+    @property
+    def userFamiliar(self):
+        return self.Familiar_tipo[0]
+    def __init__(self, username,apellidos,tipo,fechaNacimiento):
         self.username=username
+        self.apellidos=apellidos
+        self.tipo=tipo
+        self.fechaNacimient=fechaNacimiento
+    def to_dict(self):
+        user_dict={
+            "id":self.id,
+            "username":self.username,
+            "apellidos":self.apellidos,
+            "fechaNacimient":self.fechaNacimiento,
+            "email":self.Login[0].Email,
+            "tipo":None
+        }
+        if self.tipo==0:
+            user_dict["tipo"]="Paciente"
+            user_dict["Doctor"]=self.Paciente_tipo[0].doctor_dict()
+            user_dict["familiares"]=self.Paciente_tipo[0].fam_list()
+        elif self.tipo==1:
+            user_dict["tipo"]="Doctor"
+            user_dict["Cedula profecional"]:self.Doctor_tipo[0].cedula_profecional
+            user_dict["especialidad"]:self.Doctor_tipo[0].especialidad
+            user_dict["pacientes"]=self.Doctor_tipo[0].pacientes_list()
+        elif self.tipo==3:
+            user_dict["tipo"]="Familiar"
+            user_dict["paciente"]=self.Familiar_tipo[0].paciente_dict()
 
     
     
 class Login(db.Model):
     __tablename__ = "Login"
+    #id = db.Column(db.Integer,unique = True, primary_key=True)
     email = db.Column(db.String(30),nullable=False)
     password = db.Column(db.String(66),nullable=False)
-    ID_user=db.Column(db.Integer,ForeignKey('User.id'))
+    ID_user=db.Column(db.Integer,ForeignKey('User.id'),unique = True, primary_key=True)
 
-    def setEmail(self,email):
+    @property
+    def Email(self,email):
         self.email=email
-    def getEmail(self):
+    def Email(self):
         return self.email
     
     def setPassword(self,password):
@@ -43,31 +76,91 @@ class Login(db.Model):
     def checkPassword(self,password):
         return check_password_hash(self.password,password)
     
-class Historial(db.Model):
-    __tablename__ = "Historial"
-    ID_user=db.Column(db.Integer,ForeignKey('User.id'))
-    fecha = db.Column(db.DateTime,default=datetime.datetime.now)
-    
-class Onda(db.Model):
-    __tablename__ = "Onda"
-    ID_user=db.Column(db.Integer,ForeignKey('User.id'))
-    id = db.Column(db.String(30),nullable=False)
-
 class Doctor(db.Model):
     __tablename__ = "Doctor"
-    ID_user=db.Column(db.Integer,ForeignKey('User.id'))
-    cedula_profecional = db.Column(db.String(30),nullable=False)
+    cedula_profecional = db.Column(db.String(30),nullable=False,unique = True, primary_key=True)
+    ID_user=db.Column(db.Integer,ForeignKey('User.id'),unique = True)
     especialidad = db.Column(db.String(30),nullable=False)
     Pacientes=relationship("Paciente")
+    
+    def __init__(self,cedula_profecional,ID_user,especialidad="cardiologo") -> None:
+        self.cedula_profecional=cedula_profecional
+        self.ID_user=ID_user
+        self.especialidad=especialidad
+    def to_dict(self):
+        user = User.query.filter_by(id = self.ID_user).first()
+        doc_dict =user.to_dict()
+        doc_dict.pop("pacientes")
+        return doc_dict
+    def pacientes_list(self):
+        if len(self.Pacientes):
+            return [paciente.to_dict() for paciente in self.Pacientes]
+        else:
+            return "sin pacientes"
 
 class Paciente(db.Model):
     __tablename__ = "Paciente"
-    ID_user=db.Column(db.Integer,ForeignKey('User.id'))
-    cedulaDoc=db.Column(db.Integer,ForeignKey('Doctor.cedula_profecional'))
+    #id = db.Column(db.Integer,unique = True, primary_key=True)
+    ID_user=db.Column(db.Integer,ForeignKey('User.id'), primary_key=True,unique = True)
+    cedulaDoc=db.Column(db.String(30),ForeignKey('Doctor.cedula_profecional'))
     Familiares=relationship("Familiar")
 
+    def __init__(self,ID_user) -> None:
+        self.ID_user=ID_user
+    def setDoctor(self,cedulaDoc):
+        self.cedulaDoc=cedulaDoc
+    def to_dict(self):
+        user = User.query.filter_by(id = self.ID_user).first()
+        paciente_dict = user.to_dict()
+        paciente_dict.pop("Doctor")
+        return paciente_dict
+    def fam_list(self):
+        if len(self.Familiares):
+            return [familiar.to_dict() for familiar in self.Familiares]
+        else:
+            return "sin familiares"
+    def doctor_dict(self):
+        doc = Doctor.query.filter_by(cedula_profecional = self.cedulaDoc).first()
+        if not doc is None:
+            return doc.to_dict()
+        else:
+            return "Sin doctor"
 class Familiar(db.Model):
     __tablename__ = "Familiar"
+    ID_user=db.Column(db.Integer,ForeignKey('User.id'),nullable=False,unique = True, primary_key=True)
+    ID_Paciente=db.Column(db.Integer,ForeignKey('Paciente.ID_user'),nullable=True)
+    ID_Doctor=db.Column(db.Integer,ForeignKey('Paciente.cedulaDoc'),nullable=True)
+    #Paciente=relationship("Paciente")
+
+    def __init__(self,ID_user) -> None:
+        self.ID_user=ID_user
+    def setPaciente(self,ID_Paciente,ID_Doctor):
+        self.ID_Paciente=ID_Paciente
+        self.ID_Doctor=ID_Doctor
+    def to_dict(self):
+        user = User.query.filter_by(id = self.ID_user).first()
+        fam_dict=user.to_dict()
+        fam_dict.pop('paciente')
+        return fam_dict
+    
+    def paciente_dict(self):
+        paciente = Paciente.query.filter_by(ID_user = self.ID_Paciente).first()
+        if not paciente is None:
+            return paciente.to_dict()
+        else:
+            return "Sin Paciente"
+        
+'''class Historial(db.Model):
+    __tablename__ = "Historial"
+    id = db.Column(db.Integer,unique = True, primary_key=True)
     ID_user=db.Column(db.Integer,ForeignKey('User.id'))
-    id = db.Column(db.String(30),nullable=False)
-    Paciente=relationship("Paciente")
+    fecha = db.Column(db.DateTime,default=datetime.datetime.now)
+    def to_dict(self):
+        return {}
+    
+class Onda(db.Model):
+    __tablename__ = "Onda"
+    id = db.Column(db.String(30),nullable=False,unique = True, primary_key=True)
+    ID_user=db.Column(db.Integer,ForeignKey('User.id'))
+    def to_dict(self):
+        return {}'''
