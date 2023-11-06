7a2a886d3320b36d7983a47c5c84fbdf47bc1172
J4HR <92270891+J4HR@users.noreply.github.com>
2023-11-06 00:56:13 -0600

    Add files via upload

---
+++ Comunicacion/testing/testV_UDP.py
@@ -0,0 +1,10 @@
+from Connections import UDP,DEFAULT_PORT_F,DEFAULT_PORT_LMSG
+
+udp=UDP('vision')
+
+udp.sendFlag(("192.168.84.128",DEFAULT_PORT_F),'addme')
+udp.sendMSN(("192.168.84.128",DEFAULT_PORT_LMSG),'nose')
+while True:
+    input(':')
+    udp.sendFlag(("192.168.84.128",DEFAULT_PORT_F),'dropme','vision')
+    print(udp.smgList)
